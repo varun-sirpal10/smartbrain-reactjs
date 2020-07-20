9843ce0d776235e5b4a3c53fd61c860e8c07ee8d
Varun Sirpal <varunsirpal5@gmail.com>
2020-07-20 12:28:10 +0530

    Updates

---
+++ precache-manifest.56846fb213fc8843c3e263b0ed6bb56d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fec36f31750cb743321e3af6cf66f5bd",
+    "url": "/smartbrain-reactjs/index.html"
+  },
+  {
+    "revision": "6c8fd0f1839d62ddd4cc",
+    "url": "/smartbrain-reactjs/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "b8b4e966acae72687de3",
+    "url": "/smartbrain-reactjs/static/css/main.a0de1c14.chunk.css"
+  },
+  {
+    "revision": "6c8fd0f1839d62ddd4cc",
+    "url": "/smartbrain-reactjs/static/js/2.a8dc6fe8.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/smartbrain-reactjs/static/js/2.a8dc6fe8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8b4e966acae72687de3",
+    "url": "/smartbrain-reactjs/static/js/main.09f6a827.chunk.js"
+  },
+  {
+    "revision": "cbb81a3bebb03e9ead65",
+    "url": "/smartbrain-reactjs/static/js/runtime-main.63a9ecc9.js"
+  }
+]);
